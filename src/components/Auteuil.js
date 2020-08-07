7d3534dfb4264d7d264322c2915c011969b82034
Guillaume Queste <guillaume.queste@laposte.net>
2020-08-07 15:38:20 +0200

    jusqu'à Bihorel

--- src/components/Auteuil.js
+++ src/components/Auteuil.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import Zoom from 'react-reveal/Zoom'
+import Roll from 'react-reveal/Roll'
 
 class Auteuil extends Component {
     constructor(props) {
@@ -31,83 +33,117 @@ class Auteuil extends Component {
                     <div className="image">
                         <img src={requireImage(this.state.image)} alt={this.state.nom} className="tailleImage"/>
                     </div>
-                    <div className="description">
-                        <h2 className="titreCard">{this.state.nom}</h2>
-                        <ul>
-                            <li className="liCard"><strong>Piste : </strong>{this.state.piste}</li>
-                            <li className="liCard"><strong>Corde : </strong>{this.state.corde}</li>
-                            <li className="liCard"><strong>Circonference : </strong>{this.state.circonference}</li>
-                            <li className="liCard"><strong>Région : </strong>{this.state.region}</li>
-                            <li className="liCard"><strong>Département : </strong>{this.state.departement}</li>
-                            <li className="liCard"><strong>Ville à proximité : </strong>{this.state.ville}</li>
-                            <li className="liCard"><strong>Surnom : </strong>{this.state.surnom}</li>
-                            <li className="liCard"><i>&darr; + de photos &darr;</i></li>
-                        </ul>
-                    </div>
+                    <Roll right>
+                        <div className="description">
+                            <h2 className="titreCard">{this.state.nom}</h2>
+                            <ul>
+                                <li className="liCard"><strong>Piste : </strong>{this.state.piste}</li>
+                                <li className="liCard"><strong>Corde : </strong>{this.state.corde}</li>
+                                <li className="liCard"><strong>Circonference : </strong>{this.state.circonference}</li>
+                                <li className="liCard"><strong>Région : </strong>{this.state.region}</li>
+                                <li className="liCard"><strong>Département : </strong>{this.state.departement}</li>
+                                <li className="liCard"><strong>Ville à proximité : </strong>{this.state.ville}</li>
+                                <li className="liCard"><strong>Surnom : </strong>{this.state.surnom}</li>
+                                <li className="liCard"><i>&darr; + de photos &darr;</i></li>
+                            </ul>
+                        </div>
+                    </Roll>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil2.jpg')} alt='auteuil2' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil3.jpg')} alt='auteuil3' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil2.jpg')} alt='auteuil2' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil3.jpg')} alt='auteuil3' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil4.jpg')} alt='auteuil4' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil5.jpg')} alt='auteuil5' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil4.jpg')} alt='auteuil4' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil5.jpg')} alt='auteuil5' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil6.jpg')} alt='auteuil6' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil7.jpg')} alt='auteuil7' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil6.jpg')} alt='auteuil6' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil7.jpg')} alt='auteuil7' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil8.jpg')} alt='auteuil8' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil9.jpg')} alt='auteuil9' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil8.jpg')} alt='auteuil8' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil9.jpg')} alt='auteuil9' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil10.jpg')} alt='auteuil10' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil11.jpg')} alt='auteuil11' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil10.jpg')} alt='auteuil10' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil11.jpg')} alt='auteuil11' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil12.jpg')} alt='auteuil12' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil13.jpg')} alt='auteuil13' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil12.jpg')} alt='auteuil12' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil13.jpg')} alt='auteuil13' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil14.jpg')} alt='auteuil14' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil15.jpg')} alt='auteuil15' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil14.jpg')} alt='auteuil14' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil15.jpg')} alt='auteuil15' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil16.jpg')} alt='auteuil16' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/auteuil/auteuil17.jpg')} alt='auteuil17' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil16.jpg')} alt='auteuil16' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/auteuil/auteuil17.jpg')} alt='auteuil17' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
             </div>
         )
--- src/components/Bernay.js
+++ src/components/Bernay.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import Zoom from 'react-reveal/Zoom'
+import Roll from 'react-reveal/Roll'
 
 class Bernay extends Component {
     constructor(props) {
@@ -31,91 +33,129 @@ class Bernay extends Component {
                     <div className="image">
                         <img src={requireImage(this.state.image)} alt={this.state.nom} className="tailleImage"/>
                     </div>
-                    <div className="description">
-                        <h2 className="titreCard">{this.state.nom}</h2>
-                        <ul>
-                            <li className="liCard"><strong>Piste : </strong>{this.state.piste}</li>
-                            <li className="liCard"><strong>Corde : </strong>{this.state.corde}</li>
-                            <li className="liCard"><strong>Circonference : </strong>{this.state.circonference}</li>
-                            <li className="liCard"><strong>Région : </strong>{this.state.region}</li>
-                            <li className="liCard"><strong>Département : </strong>{this.state.departement}</li>
-                            <li className="liCard"><strong>Ville à proximité : </strong>{this.state.ville}</li>
-                            <li className="liCard"><strong>Surnom : </strong>{this.state.surnom}</li>
-                            <li className="liCard"><i>&darr; + de photos &darr;</i></li>
-                        </ul>
-                    </div>
+                    <Roll right>
+                        <div className="description">
+                            <h2 className="titreCard">{this.state.nom}</h2>
+                            <ul>
+                                <li className="liCard"><strong>Piste : </strong>{this.state.piste}</li>
+                                <li className="liCard"><strong>Corde : </strong>{this.state.corde}</li>
+                                <li className="liCard"><strong>Circonference : </strong>{this.state.circonference}</li>
+                                <li className="liCard"><strong>Région : </strong>{this.state.region}</li>
+                                <li className="liCard"><strong>Département : </strong>{this.state.departement}</li>
+                                <li className="liCard"><strong>Ville à proximité : </strong>{this.state.ville}</li>
+                                <li className="liCard"><strong>Surnom : </strong>{this.state.surnom}</li>
+                                <li className="liCard"><i>&darr; + de photos &darr;</i></li>
+                            </ul>
+                        </div>
+                    </Roll>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay2.jpg')} alt='bernay2' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay3.jpg')} alt='bernay3' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay2.jpg')} alt='bernay2' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay3.jpg')} alt='bernay3' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay4.jpg')} alt='bernay4' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay5.jpg')} alt='bernay5' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay4.jpg')} alt='bernay4' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay5.jpg')} alt='bernay5' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay6.jpg')} alt='bernay6' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay7.jpg')} alt='bernay7' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay6.jpg')} alt='bernay6' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay7.jpg')} alt='bernay7' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay8.jpg')} alt='bernay8' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay9.jpg')} alt='bernay9' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay8.jpg')} alt='bernay8' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay9.jpg')} alt='bernay9' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay10.jpg')} alt='bernay10' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay11.jpg')} alt='bernay11' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay10.jpg')} alt='bernay10' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay11.jpg')} alt='bernay11' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay12.jpg')} alt='bernay12' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay13.jpg')} alt='bernay13' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay12.jpg')} alt='bernay12' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay13.jpg')} alt='bernay13' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay14.jpg')} alt='bernay14' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay15.jpg')} alt='bernay15' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay14.jpg')} alt='bernay14' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay15.jpg')} alt='bernay15' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay16.jpg')} alt='bernay16' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay17.jpg')} alt='bernay17' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay16.jpg')} alt='bernay16' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay17.jpg')} alt='bernay17' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className="plusDePhotos">
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay18.jpg')} alt='bernay18' className="tailleImagePlus"/>
-                    </div>
-                    <div className="image">
-                        <img src={require('../img/bernay/bernay19.jpg')} alt='bernay19' className="tailleImagePlus"/>
-                    </div>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay18.jpg')} alt='bernay18' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
+                    <Zoom>
+                        <div className="image">
+                            <img src={require('../img/bernay/bernay19.jpg')} alt='bernay19' className="tailleImagePlus"/>
+                        </div>
+                    </Zoom>
                 </div>
             </div>
         )
